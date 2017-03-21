7a1f38957e421ad3851e00995ecb66f0f3a23d0c
esscott1 <ericscott411@gmail.com>
2017-03-21 10:27:20 -0600

    moved workflowContext.cs

---
+++ src/coreWebAPI5/Db/WorkflowContext.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace workflow.Model
+{
+	public class WorkflowContext : DbContext
+	{
+		public DbSet<BaseWorkflowItem> WorkflowDb { get; set; }
+		public DbSet<Relationship> Relationships { get; set; }
+		
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlite("Filename=./Repository.db", x => x.SuppressForeignKeyEnforcement());
+				
+
+		}
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<BaseWorkflowItem>()
+				.HasKey(w => new { w.Name, w.DerivedType });
+				
+			modelBuilder.Entity<Relationship>()
+				.HasKey(r => new { r.TransactionName, r.TrackableName });
+				
+		}
+	}
+
+	
+}
