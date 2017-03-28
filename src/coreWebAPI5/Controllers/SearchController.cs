b82520a6405de2bc90233037bd1dd4ea46c49d15
esscott1 <ericscott411@gmail.com>
2017-03-28 13:21:19 -0600

    added search controller with simple search working

---
+++ src/coreWebAPI5/Controllers/SearchController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using workflow.Model;
+using workflow.Db;
+using Microsoft.AspNetCore.WebUtilities;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace workflow.Controllers
+{
+	[Route("api/[controller]")]
+	public class SearchController : Controller
+    {
+		public SearchController(IWorkflowRepository workflow)
+		{
+			Repository = workflow;
+		}
+		public IWorkflowRepository Repository { get; set; }
+		// GET: /<controller>/
+		[HttpGet]
+		public IActionResult Search(string q, string id)
+		{
+			// t = type of object
+			// name = name of object
+			// 
+			//	var workflow = Repository.Find<Workflow>(id);
+			//return Json(q);
+			Dictionary<string, Microsoft.Extensions.Primitives.StringValues> dic = 
+				new Dictionary<string, Microsoft.Extensions.Primitives.StringValues>();
+
+			dic = QueryHelpers.ParseQuery(Request.QueryString.ToString());
+			SearchEngine se = new Db.SearchEngine(Repository);
+
+			Workflow result = se.Search<Workflow>(dic);
+			
+			return Json(result);
+			//return Json(HttpContext.Request.Query.ToList());
+		}
+	}
+}
---
+++ src/coreWebAPI5/Db/SearchEngine.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using workflow.Model;
+
+namespace workflow.Db
+{
+    public class SearchEngine
+    {
+		private IWorkflowRepository Repository { get; set; }
+		public SearchEngine(IWorkflowRepository repository)
+		{
+			Repository = repository;
+		}
+
+		public T Search<T>(Dictionary<string, 
+			Microsoft.Extensions.Primitives.StringValues> values) where T : BaseWorkflowItem
+		{
+			string q = values.FirstOrDefault(v => v.Key == "id").Value;
+			return Repository.Find<T>(q);
+			
+
+		}
+
+		
+
+	
+    }
+}
