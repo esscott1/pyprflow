52e064062506bfc788d7593e8fc76869f453b4b9
esscott1 <ericscott411@gmail.com>
2017-06-06 23:36:12 -0600

    more null exception fixing

--- src/pyprflow/Db/WorkflowContext.cs
+++ src/pyprflow/Db/WorkflowContext.cs
@@ -24,13 +24,16 @@ namespace pyprflow.Model
             Helpers.IConnectionString Iconn = null;
             Iconn = Helpers.ConnectionStringFactory.GetConnetionString();
             string conn = Iconn.ConnectionString();
-            switch (Environment.GetEnvironmentVariable("DatabaseType").ToLower())
+            switch (Environment.GetEnvironmentVariable("DatabaseType"))
             {
-                case ("mssql"):
+                case "mssql":
                     optionsBuilder.UseSqlServer(conn);
                     break;
-                case ("mssql2017"):
+                case "mssql2017":
                     optionsBuilder.UseSqlServer(conn);
+                    break;
+                case null:
+                    optionsBuilder.UseSqlite(conn, x => x.SuppressForeignKeyEnforcement());
                     break;
                 default:
                     optionsBuilder.UseSqlite(conn, x => x.SuppressForeignKeyEnforcement());
