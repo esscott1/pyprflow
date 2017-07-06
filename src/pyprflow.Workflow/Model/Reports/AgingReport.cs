ce8d3c9c0b8c77033a411ceb6094efae429ec1e1
esscott1 <ericscott411@gmail.com>
2017-07-05 20:29:52 -0600

    added reports controller and ReportEngine capabilities

--- src/pyprflow.Workflow/Model/Reports/AgingReport.cs
+++ src/pyprflow.Workflow/Model/Reports/AgingReport.cs
@@ -4,7 +4,25 @@ using System.Text;
 
 namespace pyprflow.Workflow.Model.Reports
 {
-    class AgingReportcs
+    public class AgingReport : IReport
     {
+        public AgingReport()
+        {
+
+        }
+        public string TrackableName { get; set; }
+        public TimeSpan IdileDuration { get; set; }
+        public TimeSpan AssignedDuration { get; set; }
+        public TimeSpan NodeDuration { get; set; }
+
+        public IReport Run() {
+            return new AgingReport()
+            {
+                TrackableName = "sampletrackable",
+                IdileDuration = new TimeSpan(4, 30, 30),
+                AssignedDuration = new TimeSpan(5, 45, 45),
+                NodeDuration = new TimeSpan(6, 15, 30)
+            };
+        }
     }
 }
